500ed485c8b721bcefa83153d6db8a820c6bd5aa
malopo <loganwapo113@gmail.com>
2024-04-11 12:57:13 -0500

    JS exercise to create predictive search bar for fruit names

---
+++ script.js
@@ -0,0 +1,25 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+function search(str) {
+	return fruit.filter(fruit => fruit.toLowerCase().includes(str.toLowerCase()));
+}
+
+function searchHandler(e) {
+	let inputVal = e.target.value;
+	inputVal.length > 0 ? showSuggestions(search(inputVal), inputVal) : suggestions.innerHTML = "";
+}
+
+function showSuggestions(results, inputVal) {
+	results.length > 0 ? suggestions.classList.add('has-suggestions') : suggestions.classList.remove('has-suggestions');
+	suggestions.innerHTML = results.map(result => `<li>${result}</li>`).join('');
+}
+
+function useSuggestion(e) {
+	e.target.tagName === 'LI' ? (input.value = e.target.textContent, suggestions.innerHTML = "") : null;
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
